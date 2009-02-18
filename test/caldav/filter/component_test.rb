9cf468be15aa6dabc6ec321610e33d3196ef8e6b
John Hwang <jhwang@mutuallyhuman.com>
2009-02-18 16:48:59 -0500

    Converted test_build_xml_without_child and test_build_xml_with_child to use Nokogiri

--- test/caldav/filter/component_test.rb
+++ test/caldav/filter/component_test.rb
@@ -36,16 +36,14 @@ class ComponentTest < Test::Unit::TestCase
   
   def test_build_xml_without_child
     @filter.name = 'hello'
-    xml = REXML::Document.new(@filter.to_xml)
-    assert_kind_of REXML::Element, REXML::XPath.match( xml, "cal:comp-filter[@name='hello']").first
+    xml = Nokogiri::XML.parse(@filter.to_xml)
+    assert_kind_of Nokogiri::XML::Element, xml.search("//comp-filter[@name='hello']").first
   end
   
   def test_build_xml_with_child
     @filter.name = 'hello'
     @filter.child = CalDAV::Filter::Component.new('child name')
-    xml = REXML::Document.new(@filter.to_xml)
-    child_xml = REXML::XPath.match( xml, "cal:comp-filter[@name='hello']").first
-    assert_kind_of REXML::Element, child_xml
-    assert !xml.root.elements.empty?
+    xml = Nokogiri::XML.parse(@filter.to_xml)
+    assert_kind_of Nokogiri::XML::Element, xml.search("//comp-filter[@name='hello']").first
   end
 end
